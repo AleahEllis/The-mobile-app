7b140632105bc1c51a3a3398411af08cfb822a4f
Aleah Ellis <aleah.is@icloud.com>
2016-03-14 14:34:09 -0400

    beer list atwater and batch

---
+++ scripts/beerList.js
@@ -0,0 +1,147 @@
+var beerArray=[ //aide from name description and maker we could include alchohle content. good for filtering
+	{
+		Name:"Dirty Blonde",
+		Description:"a beer with a pale golden hue, sweeter maltiness with light fruitynotes. Crushed oraangepeel and Coriander, clean heady aroma"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"Vanilla Java Porter",
+		Description:"A copper cored Englishstyle porter with unmistakeble notes of vanilla and coffee. Brewed cold to elimnates burnt flavors."
+		Maker:"Atwater"
+	},
+	
+	{
+		Name:"Decandent Dark Chocolate",
+		Description:"Nutty, round mouthfeel with an a blend of chocolate extract added for a rich, not-too-sweet mellow flavor profile"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"Detroit Pale Ale",
+		Description:"A unique and diverse beer bringing in flavors from English and German malts."
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"D-light",
+		Description:"Clean, crisp and lightly floral"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"Grand Circus IPA",
+		Description:"Crisp, clean and hoppy, lightly bready with notes of citrus and caramel"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"Hop-A-Peel",
+		Description:"A Super Cascade of American and Chinook hops makes gives you a full IPA experience"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"Michelada",
+		Description:"Atwater Lager + Bloody Mary mix = party time"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"Purple Gang Pilsner",
+		Description:"lightly malty and subtly sweet"
+		Maker:"Atwater"
+	},
+
+	{
+		Name:"VooDoo Vator",
+		Description:"This exceptionally smooth caremal malt carries a crisp nutty flavor, along with a high dose of liquid courage"
+		Maker:"Atwater"
+	},
+//here starts Batch - may be able to easily get API for this https://www.beermenus.com/beers/120018-batch-wheated-session-ipa -their info on Atwter sucked though
+	{
+		Name:"Anton's Animal's II",
+		Description:"A refreshing Berliner Weisse, served with a house-made Blueberry Anise Hyssop syrup"
+		Maker:"Batch"
+	},
+
+	{
+		Name:"1400 Porter",
+		Description:"English syle robust porter, roasted malt character, with caramel malt undertones."
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Antwerp's Placebo II",
+		Description:"Big Beautiful Belgian Dark Strong Ale. Special bottle release on our 1 year anniversary!"
+		Maker:"Batch"
+	},
+	
+	{
+		Name:"Batch Empire Pale Ale",
+		Description:"American Pale Ale featuring Michigan grown Empire Hops. Light and crisp with a nice floral/fruity finish."
+		Maker:"Batch"
+	},
+
+	{
+		Name:"High End Theory",
+		Description:"Black IPA…Low End Theory’s bigger brother."
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Low End Theory",
+		Description:"Big Dark Black IPA with copious amounts of Michigan grown Chinook and Cascade hops"
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Milk Goblin II",
+		Description:"Milk Stout which is nice and roasty with lactose."
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Batch Ned the Wheat Wino",
+		Description:"Our first wheat wine…with bready, oaky, alcohol heat characters. Good Times."
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Old Witty Bastard II",
+		Description:"A delicious Belgian style witbier featuring coriander and orange peel. Yum!"
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Son of a Batch II",
+		Description:"Nicely balanced double IPA, brewed with cascade and centennial hops."
+		Maker:"Batch"
+	},
+
+	{
+		Name:"The Noive",
+		Description:"The Noive of some people. A delightfully refreshing kolsch. Nice and clean, easy drinking, and the right choice for anytime of the day!"
+		Maker:"Batch"
+	},
+	
+	{
+		Name:"Unrequited",
+		Description:"A dry/semi-dry Belgian Sparkling Ale featuring fruity and spicy fermentation character with low hop and low malt presence. It’s all about the Belgian yeast, baby!",
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Wheated Kolsh",
+		Description:"Light Refreshing Wheat/Kolsch Hybrid",
+		Maker:"Batch"
+	},
+
+	{
+		Name:"Wheated Session IPA",
+		Description:"Wheat based session IPA. Dry hopped with Cascade and Centennial hops for intense hop aromatics.",
+		Maker:"Batch"
+	}
+];
+
+module.exports = beerArray; //not being exported currently, plan to export both to beerlis.html as well as on the beer rec page
