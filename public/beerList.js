daa7082e9afb07907a15e3ce656dc3cbf4f2fd91
Aleah Ellis <aleah.is@icloud.com>
2016-03-15 16:42:55 -0400

    recipie service

--- public/beerList.js
+++ public/beerList.js
@@ -1,219 +1,219 @@
-var beerArray=[ //aide from name description and maker we could include alcohol content. good for filtering
-	{
-		Name:"Dirty Blonde",
-        Style:"Wheat Spiced Ale",
-		Description:"A beer with a pale golden hue, sweeter maltiness with light fruity notes. Crushed orange peel and coriander, clean heady aroma.",
-		Maker:"Atwater",
-        Glass:"Stein",
-        Temperature:"48-50°F"
-	},
+// var beerArray=[ //aide from name description and maker we could include alcohol content. good for filtering
+// 	{
+// 		Name:"Dirty Blonde",
+//         Style:"Wheat Spiced Ale",
+// 		Description:"A beer with a pale golden hue, sweeter maltiness with light fruity notes. Crushed orange peel and coriander, clean heady aroma.",
+// 		Maker:"Atwater",
+//         Glass:"Stein",
+//         Temperature:"48-50°F"
+// 	},
 
-	{
-		Name:"Vanilla Java Porter",
-        Style:"English Porter Spiced Ale",
-		Description:"A copper cored porter with unmistakeable notes of vanilla and coffee. Brewed cold to eliminates burnt flavors.",
-		Maker:"Atwater",
-        Glass:"Pint",
-        Temperature:"50-55°F"
-	},
+// 	{
+// 		Name:"Vanilla Java Porter",
+//         Style:"English Porter Spiced Ale",
+// 		Description:"A copper cored porter with unmistakeable notes of vanilla and coffee. Brewed cold to eliminates burnt flavors.",
+// 		Maker:"Atwater",
+//         Glass:"Pint",
+//         Temperature:"50-55°F"
+// 	},
 	
-	{
-		Name:"Decadent Dark Chocolate",
-        Style:"Strong Brown Chocolate Ale",
-		Description:"Nutty, round mouthfeel with an a blend of chocolate extract added for a rich, not-too-sweet mellow flavor profile.",
-		Maker:"Atwater",
-        Glass:"Nonic",
-        Temperature:"50-55°F"
-	},
+// 	{
+// 		Name:"Decadent Dark Chocolate",
+//         Style:"Strong Brown Chocolate Ale",
+// 		Description:"Nutty, round mouthfeel with an a blend of chocolate extract added for a rich, not-too-sweet mellow flavor profile.",
+// 		Maker:"Atwater",
+//         Glass:"Nonic",
+//         Temperature:"50-55°F"
+// 	},
 
-	{
-		Name:"Detroit Pale Ale",
-        Style:"Pale Ale",
-		Description:"A unique and diverse beer bringing in flavors from English and German malts.",
-		Maker:"Atwater",
-        Glass:"Becker",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"Detroit Pale Ale",
+//         Style:"Pale Ale",
+// 		Description:"A unique and diverse beer bringing in flavors from English and German malts.",
+// 		Maker:"Atwater",
+//         Glass:"Becker",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"D-light",
-        Style:"Kolsch",
-		Description:"Clean, crisp and lightly floral.",
-		Maker:"Atwater",
-        Glass:"Stange",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"D-light",
+//         Style:"Kolsch",
+// 		Description:"Clean, crisp and lightly floral.",
+// 		Maker:"Atwater",
+//         Glass:"Stange",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"Grand Circus IPA",
-        Style:"IPA",
-		Description:"Crisp, clean and hoppy, lightly bready with notes of citrus and caramel.",
-		Maker:"Atwater",
-        Glass:"Seidel",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"Grand Circus IPA",
+//         Style:"IPA",
+// 		Description:"Crisp, clean and hoppy, lightly bready with notes of citrus and caramel.",
+// 		Maker:"Atwater",
+//         Glass:"Seidel",
+//         Temperature:"45-50°F"
+// 	},
 
-	{
-		Name:"Hop-A-Peel",
-        Style:"American IPA",
-		Description:"A Super Cascade of American and Chinook hops makes gives you a full IPA experience.",
-		Maker:"Atwater",
-        Glass:"Seidel",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"Hop-A-Peel",
+//         Style:"American IPA",
+// 		Description:"A Super Cascade of American and Chinook hops makes gives you a full IPA experience.",
+// 		Maker:"Atwater",
+//         Glass:"Seidel",
+//         Temperature:"45-50°F"
+// 	},
 
-	{
-		Name:"Michelada",
-        Style:"Michelada",
-		Description:"Atwater Lager + Bloody Mary mix = party time.",
-		Maker:"Atwater",
-        Glass:"Becker",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"Michelada",
+//         Style:"Michelada",
+// 		Description:"Atwater Lager + Bloody Mary mix = party time.",
+// 		Maker:"Atwater",
+//         Glass:"Becker",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"Purple Gang Pilsner",
-        Style:"German Pilsner",
-		Description:"Lightly malty and subtly sweet.",
-		Maker:"Atwater",
-        Glass:"Flute",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"Purple Gang Pilsner",
+//         Style:"German Pilsner",
+// 		Description:"Lightly malty and subtly sweet.",
+// 		Maker:"Atwater",
+//         Glass:"Flute",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"VooDoo Vator",
-        Style:"DOPPELBOCK",
-		Description:"This exceptionally smooth caramel malt carries a crisp nutty flavor, along with a high dose of liquid courage.",
-		Maker:"Atwater",
-        Glass:"Pilsener",
-        Temperature:"45-50°F"
-	},
-//here starts Batch - may be able to easily get API for this https://www.beermenus.com/beers/120018-batch-wheated-session-ipa -their info on Atwter sucked though
-	{
-		Name:"Anton's Animal's II",
-        Style:"Berliner Weisse",
-		Description:"Refreshing, tart, sour and acidic, with a lemony-citric fruit sharpness and almost no hop bitterness.",
-		Maker:"Batch",
-        Glass:"Nonic",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"VooDoo Vator",
+//         Style:"DOPPELBOCK",
+// 		Description:"This exceptionally smooth caramel malt carries a crisp nutty flavor, along with a high dose of liquid courage.",
+// 		Maker:"Atwater",
+//         Glass:"Pilsener",
+//         Temperature:"45-50°F"
+// 	},
+// //here starts Batch - may be able to easily get API for this https://www.beermenus.com/beers/120018-batch-wheated-session-ipa -their info on Atwter sucked though
+// 	{
+// 		Name:"Anton's Animal's II",
+//         Style:"Berliner Weisse",
+// 		Description:"Refreshing, tart, sour and acidic, with a lemony-citric fruit sharpness and almost no hop bitterness.",
+// 		Maker:"Batch",
+//         Glass:"Nonic",
+//         Temperature:"45-50°F"
+// 	},
 
-	{
-		Name:"1400 Porter",
-        Style:"English Porter",
-		Description:"English style robust porter, roasted malt character, with caramel malt undertones.",
-		Maker:"Batch",
-        Glass:"Pint",
-        Temperature:"50-55°F"
-	},
+// 	{
+// 		Name:"1400 Porter",
+//         Style:"English Porter",
+// 		Description:"English style robust porter, roasted malt character, with caramel malt undertones.",
+// 		Maker:"Batch",
+//         Glass:"Pint",
+//         Temperature:"50-55°F"
+// 	},
 
-	{
-		Name:"Antwerp's Placebo II",
-        Style:"Belgian Dark Strong Ale",
-		Description:"Big Beautiful Belgian Ale. Special bottle release on our 1 year anniversary!",
-		Maker:"Batch",
-        Glass:"Snifter",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"Antwerp's Placebo II",
+//         Style:"Belgian Dark Strong Ale",
+// 		Description:"Big Beautiful Belgian Ale. Special bottle release on our 1 year anniversary!",
+// 		Maker:"Batch",
+//         Glass:"Snifter",
+//         Temperature:"45-50°F"
+// 	},
 	
-	{
-		Name:"Batch Empire Pale Ale",
-        Style:"American Pale Ale",
-		Description:"Featuring Michigan grown Empire Hops. Light and crisp with a nice floral/fruity finish.",
-		Maker:"Batch",
-        Glass:"Becker",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"Batch Empire Pale Ale",
+//         Style:"American Pale Ale",
+// 		Description:"Featuring Michigan grown Empire Hops. Light and crisp with a nice floral/fruity finish.",
+// 		Maker:"Batch",
+//         Glass:"Becker",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"High End Theory",
-        Style:"Black IPA",
-		Description:"Low End Theory’s bigger brother.",
-		Maker:"Batch",
-        Glass:"Becker",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"High End Theory",
+//         Style:"Black IPA",
+// 		Description:"Low End Theory’s bigger brother.",
+// 		Maker:"Batch",
+//         Glass:"Becker",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"Low End Theory",
-        Style:"Dark Black IPA",
-		Description:"Copious amounts of Michigan grown Chinook and Cascade hops.",
-		Maker:"Batch",
-        Glass:"Becker",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"Low End Theory",
+//         Style:"Dark Black IPA",
+// 		Description:"Copious amounts of Michigan grown Chinook and Cascade hops.",
+// 		Maker:"Batch",
+//         Glass:"Becker",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"Milk Goblin II",
-        Style:"Milk Stout",
-		Description:"Nice, creamy and roasty flavors.",
-		Maker:"Batch",
-        Glass:"Nonic",
-        Temperature:"50-55°F"
-	},
+// 	{
+// 		Name:"Milk Goblin II",
+//         Style:"Milk Stout",
+// 		Description:"Nice, creamy and roasty flavors.",
+// 		Maker:"Batch",
+//         Glass:"Nonic",
+//         Temperature:"50-55°F"
+// 	},
 
-	{
-		Name:"Batch Ned the Wheat Wino",
-        Style:"Wheat Wine",
-		Description:"Bready, oaky, alcohol heat characters. Good Times.",
-		Maker:"Batch",
-        Glass:"Snifter",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"Batch Ned the Wheat Wino",
+//         Style:"Wheat Wine",
+// 		Description:"Bready, oaky, alcohol heat characters. Good Times.",
+// 		Maker:"Batch",
+//         Glass:"Snifter",
+//         Temperature:"45-50°F"
+// 	},
 
-	{
-		Name:"Old Witty Bastard II",
-        Style:"Belgian Witbier",
-		Description:"A delicious witbier featuring coriander and orange peel. Yum!",
-		Maker:"Batch",
-        Glass:"Pilsener",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"Old Witty Bastard II",
+//         Style:"Belgian Witbier",
+// 		Description:"A delicious witbier featuring coriander and orange peel. Yum!",
+// 		Maker:"Batch",
+//         Glass:"Pilsener",
+//         Temperature:"45-50°F"
+// 	},
 
-	{
-		Name:"Son of a Batch II",
-        Style:"Double IPA",
-		Description:"Nicely balanced, brewed with cascade and centennial hops.",
-		Maker:"Batch",
-        Glass:"Snifter",
-        Temperature:"50-55°F"
-	},
+// 	{
+// 		Name:"Son of a Batch II",
+//         Style:"Double IPA",
+// 		Description:"Nicely balanced, brewed with cascade and centennial hops.",
+// 		Maker:"Batch",
+//         Glass:"Snifter",
+//         Temperature:"50-55°F"
+// 	},
 
-	{
-		Name:"The Noive",
-        Style:"Kölsch",
-		Description:"The Noive of some people. Delightful and refreshing. Nice and clean, easy drinking, and the right choice for anytime of the day!",
-		Maker:"Batch",
-        Glass:"Stange",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"The Noive",
+//         Style:"Kölsch",
+// 		Description:"The Noive of some people. Delightful and refreshing. Nice and clean, easy drinking, and the right choice for anytime of the day!",
+// 		Maker:"Batch",
+//         Glass:"Stange",
+//         Temperature:"40-45°F"
+// 	},
 	
-	{
-		Name:"Unrequited",
-        Style:"Belgian Sparkling Ale",
-		Description:"A dry/semi-dry sparkling ale featuring fruity and spicy fermentation character with low hop and low malt presence. It’s all about the Belgian yeast, baby!",
-		Maker:"Batch",
-        Glass:"Pint",
-        Temperature:"45-50°F"
-	},
+// 	{
+// 		Name:"Unrequited",
+//         Style:"Belgian Sparkling Ale",
+// 		Description:"A dry/semi-dry sparkling ale featuring fruity and spicy fermentation character with low hop and low malt presence. It’s all about the Belgian yeast, baby!",
+// 		Maker:"Batch",
+//         Glass:"Pint",
+//         Temperature:"45-50°F"
+// 	},
 
-	{
-		Name:"Wheated Kölsch",
-        Style:"Wheat Kölsch",
-		Description:"Light Refreshing Wheat/Kölsch Hybrid",
-		Maker:"Batch",
-        Glass:"Stange",
-        Temperature:"40-45°F"
-	},
+// 	{
+// 		Name:"Wheated Kölsch",
+//         Style:"Wheat Kölsch",
+// 		Description:"Light Refreshing Wheat/Kölsch Hybrid",
+// 		Maker:"Batch",
+//         Glass:"Stange",
+//         Temperature:"40-45°F"
+// 	},
 
-	{
-		Name:"Wheated Session IPA",
-        Style:"Wheat IPA",
-		Description:"Dry hopped with Cascade and Centennial hops for intense hop aromatics.",
-		Maker:"Batch",
-        Glass:"Pint",
-        Temperature:"45-50°F"
-	}
-];
+// 	{
+// 		Name:"Wheated Session IPA",
+//         Style:"Wheat IPA",
+// 		Description:"Dry hopped with Cascade and Centennial hops for intense hop aromatics.",
+// 		Maker:"Batch",
+//         Glass:"Pint",
+//         Temperature:"45-50°F"
+// 	}
+// ];
 
-module.exports = beerArray; 
+// module.exports = beerArray; 
