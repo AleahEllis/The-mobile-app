cc7eff7ff8b1acdc712db6bae196956818eb8970
Michael Sandula <msandula@gmail.com>
2016-03-16 15:27:48 -0400

    Deleted beerList files -- no longer needed

--- public/scripts/controllers/beerlistController.js
+++
@@ -1,9 +0,0 @@
-var app=angular.module('beerApp');
-	app.controller('beerListCont',['$scope', 'beerListService', function($scope, beerListService){
-		beerListService.then(function(response){
-
-			$scope.showBeer=response.data;
-
-		});
-		
-	}]);
