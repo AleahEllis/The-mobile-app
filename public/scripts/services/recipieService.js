7083290dd118c094171f0c552f6dd9385bd2b41b
Aleah Ellis <aleah.is@icloud.com>
2016-03-16 12:57:40 -0400

    merge conflicts

--- public/scripts/services/recipieService.js
+++ public/scripts/services/recipieService.js
@@ -1,45 +1,20 @@
-http://food2fork.com/about/api
-
 angular.module('beerApp', []);
 
-.controller('recipieController', function($scope, $http){
-    $scope.$watch('search', function() {
-      fetch();
+app.controller("recepieCtrl", function($scope, $http) {
+  $http.get('data/posts.json').
+    success(function(data, status, headers, config) {
+      $scope.posts = data;
+    })
+    .error(function(data, status, headers, config) {
+      // log error
     });
+});
 
-    $scope.search = "a view";
-
-    // search request
-    key: c149f9bfb783bcd836c5dde3bc12d679
-    q: flavor
-	sort: sort=r
-
-	// The response is json encoded
-	// count: Number of recipes in result (Max 30) WE WANT 3 ONLY
-	recipes: List of Recipe Parameters ->
-	image_url: URL of the image
-	source_url: Original Url of the recipe on the publishers site
-	f2f_url: Url of the recipe on Food2Fork.com
-	title: Title of the recipe
-	publisher: Name of the Publisher
-	publisher_url: Base url of the publisher
-	social_rank: The Social Ranking of the Recipe (As determined by our Ranking Algorithm)
-	page: The page number that is being returned (To keep track of concurrent requests)
-
-
-    function fetch(){
-      $http.get("http://food2fork.com/api/search" + $scope.search + "")
-      .then(function(response){ $scope.details = response.data; });
-
-      $http.get("" + $scope.search)
-      .then(function(response){ $scope.related = response.data; });
-    }
-
-    $scope.update = function(?){
-      $scope.search = ?.Title;
-    };
-
-    $scope.select = function(){
-      this.setSelectionRange(0, this.value.length);
-    }
-  });
+// in the html-probs the view
+// <body ng-app="beerApp">
+//   <div ng-controller="recepieCtrl">
+//     <ul ng-repeat="post in posts">
+//       <li>{{post.title}}</li>
+//     </ul>
+//   </div>
+// </body>
