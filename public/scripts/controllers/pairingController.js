f2238e69c20bcadf22753d6b7404ded87dcfc1ef
Michael Sandula <msandula@gmail.com>
2016-03-17 12:44:21 -0400

    When user selects beer from dropdown, an object gets returned

--- public/scripts/controllers/pairingController.js
+++ public/scripts/controllers/pairingController.js
@@ -1,8 +1,17 @@
-angular.module('beerApp')
-.controller('pairingController', ['$scope', 'pairingService', function($scope, pairingService) {
+var app = angular.module('beerApp')
+app.controller('pairingController', ['$scope', 'pairingService', function($scope, pairingService) {
 	$scope.breweries  = pairingService.breweries;
 	$scope.atwater    = pairingService.atwater;
 	$scope.batch      = pairingService.batch;
 	$scope.choice2    = pairingService.choice2;
 	$scope.selectBeer = pairingService.selectBeer;
+	$scope.selectedBeer = pairingService.selectedBeer;
+	$scope.usersBeerChoice = pairingService.usersBeerChoice;
+	$scope.sbIndex = pairingService.sbIndex;
 }]);
+
+// app.directive('pairingDirective', function() {
+// 	return {
+// 		templateUrl: 'views/pairing.html'
+// 	};
+// });
