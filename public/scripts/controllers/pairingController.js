24fbb6ebc741667953c0650fd562a9fd2a57d6c7
Michael Sandula <msandula@gmail.com>
2016-03-17 16:05:57 -0400

    Dropdowns now work as they should

--- public/scripts/controllers/pairingController.js
+++ public/scripts/controllers/pairingController.js
@@ -1,17 +1,4 @@
 var app = angular.module('beerApp')
 app.controller('pairingController', ['$scope', 'pairingService', function($scope, pairingService) {
-	$scope.breweries  = pairingService.breweries;
-	$scope.atwater    = pairingService.atwater;
-	$scope.batch      = pairingService.batch;
-	$scope.choice2    = pairingService.choice2;
-	$scope.selectBeer = pairingService.selectBeer;
-	$scope.selectedBeer = pairingService.selectedBeer;
-	$scope.usersBeerChoice = pairingService.usersBeerChoice;
-	$scope.sbIndex = pairingService.sbIndex;
-}]);
 
-// app.directive('pairingDirective', function() {
-// 	return {
-// 		templateUrl: 'views/pairing.html'
-// 	};
-// });
+}]);
--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,13 +1,13 @@
 angular.module('beerApp', ['ngRoute'])
-.config(function($routeProvider) {
-  $routeProvider
-    .when('/', {
-      templateUrl: 'views/landingPage.html',
-      controller: 'landingPageController'
-    })
-    .when('/views/pairing', {
-      templateUrl: 'views/pairing.html',
-      controller: 'pairingController'
-    })
-    .otherwise({ redirectTo: '/' });
-});
+// .config(function($routeProvider) {
+//   $routeProvider
+//     .when('/', {
+//       templateUrl: 'views/landingPage.html',
+//       controller: 'landingPageController'
+//     })
+//     .when('/views/pairing', {
+//       templateUrl: 'views/pairing.html',
+//       controller: 'pairingController'
+//     })
+//     .otherwise({ redirectTo: '/' });
+// });
