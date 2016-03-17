0e3c74949585b11fb8df15e6ee974026eaccfeb1
Aleah Ellis <aleah.is@icloud.com>
2016-03-17 11:28:27 -0400

    background change

--- public/scripts/services/recipeService.js
+++ public/scripts/services/recipeService.js
@@ -14,14 +14,14 @@ angular.module('beerApp')
 	}])
 
 
-angular.module('beerApp')
-	.controller('recipeController', function($scope, $location, $http){
-		$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=<required>&number=3&ranking=1")
-		.header("X-Mashape-Key", "Ae6oX6OgLBmsh87xZW56z2AdA3Z8p1IWFf2jsngj2S7si3ElSe")
-		.header("Accept", "application/json")
-		.end(function (result){
-			return result
-		})
-		// console.log(result)
-	});
-{{3+3}}
+// angular.module('beerApp')
+// 	.controller('recipeController', function($scope, $location, $http){
+// 		$http.get("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=<required>&number=3&ranking=1")
+// 		// .header("X-Mashape-Key", "Ae6oX6OgLBmsh87xZW56z2AdA3Z8p1IWFf2jsngj2S7si3ElSe")
+// 		// .header("Accept", "application/json")
+// 		// .end(function (result){
+// 		// 	return result
+// 		// })
+// 		// console.log(result)
+// 	});
+
