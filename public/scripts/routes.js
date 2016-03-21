b09796ea6791e801ecb554cd910b078f9672b836
Aleah Ellis <aleah.is@icloud.com>
2016-03-21 14:42:19 -0400

    footer with lightboxes

--- public/scripts/routes.js
+++ public/scripts/routes.js
@@ -1,4 +1,40 @@
 angular.module('beerApp', []);
+//contains lightbox logic for footer
+//ui bootstrap models could potentially make this simpler
+	window.document.onkeydown = function (e)
+		{
+		    if (!e){
+		        e = event;
+		    }
+		    if (e.keyCode === 27){
+		        contact_close();
+		    	about_close();
+		    }
+		}
 
+
+	function contact_open(){
+	    window.scrollTo(0,0);
+	    document.getElementById('contact').style.display='block';
+	    document.getElementById('contactFade').style.display='block';  
+	};
+
+	function about_open(){
+	    window.scrollTo(0,0);
+	    document.getElementById('about').style.display='block';
+	    document.getElementById('aboutFade').style.display='block';  
+	};
+
+	
+	function contact_close(){
+	    document.getElementById('contact').style.display='none';
+	    document.getElementById('contactFade').style.display='none';
+	};
+
+
+	function about_close(){
+	    document.getElementById('about').style.display='none';
+	    document.getElementById('aboutFade').style.display='none';
+	};
 // pairingController.$inject=['$scope','pairingService','recipeService'];
 // mainController.$inject=['$scope','pairingService','recipeService'];
